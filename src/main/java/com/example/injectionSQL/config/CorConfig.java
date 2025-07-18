3c7134ba5ca98f8027e81a16565c8cbea790ffd5
Petriv2004 <felipetrivinogarzon@gmail.com>
2025-07-18 13:38:48 -0500

    update method

--- src/main/java/com/example/injectionSQL/config/CorConfig.java
+++ src/main/java/com/example/injectionSQL/config/CorConfig.java
@@ -19,4 +19,23 @@ public class CorConfig {
             }
         };
     }
+    //Cuando tenga un frontend
+/** 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                            "http://localhost:3000",         // Desarrollo local
+                            "https://frontend.wagglo.com"    // Producción real
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("Authorization", "Content-Type")
+                        .allowCredentials(true);
+            }
+        };
+    }
+    */
 }
